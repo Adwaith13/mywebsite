575508641e34217bca4a292a320f7797b15924f5
Adwaith Nair <adwaith1310@gmail.com>
2023-12-10 01:13:43 +0530

    styled contact form

--- mywebsite/src/app/Components/ContactMe.jsx
+++ mywebsite/src/app/Components/ContactMe.jsx
@@ -2,54 +2,61 @@
 import React from "react";
 import { useForm, ValidationError } from "@formspree/react";
 import Thanks from "./Thanks";
-import Image from "next/image";
+import contactFormStyle from "../styles/contactForm.module.css";
 
 export default function ContactMe() {
   const [state, handleSubmit] = useForm("mvojebkb");
   if (state.succeeded) {
     return <Thanks />;
-}
+  }
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Your Name"
-          required
-        ></input>
-         <ValidationError
-        prefix="Name"
-        field="name"
-        errors={state.errors}
-      />
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Enter your email"
-          required
-        ></input>
-         <ValidationError
-        prefix="Email"
-        field="email"
-        errors={state.errors}
-      />
-        <textarea
-          id="message"
-          name="message"
-          placeholder="Enter your Message"
-          required
-        ></textarea>
-         <ValidationError
-        prefix="Message"
-        field="message"
-        errors={state.errors}
-      />
-        <button type="submit" disabled={state.submitting}>Send</button>
-      </form>
+    <div className={contactFormStyle.formContainer}>
+      <h1 className={contactFormStyle.formHead}>Contact Me</h1>
+      <div className={contactFormStyle.form}>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Your Name"
+            required
+            className={contactFormStyle.input}
+          ></input>
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
+          <br />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email"
+            required
+            className={contactFormStyle.input}
+          ></input>
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <br />
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Your Message"
+            required
+            rows={12}
+            className={contactFormStyle.textarea}
+          ></textarea>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <br />
+          <button
+            type="submit"
+            disabled={state.submitting}
+            className={contactFormStyle.button}
+          >
+            Let's Connect
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
