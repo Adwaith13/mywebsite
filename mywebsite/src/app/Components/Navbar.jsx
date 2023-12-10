922375c2b9b38ad3cbb0fae9f03ebdd7f35d9b89
Adwaith Nair <adwaith1310@gmail.com>
2023-12-11 00:00:22 +0530

    updates

--- mywebsite/src/app/Components/Navbar.jsx
+++ mywebsite/src/app/Components/Navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
 
   const handleResumeClick = () => {
     const resumeURL =
-      "https://docs.google.com/document/d/1ecRc2qZ4cP2iZANH1wEu1-dbe-qFMPlPWwAxmCHncaw/edit?usp=sharing";
+      "https://drive.google.com/file/d/1Vg0is2UvcanCrdYrq0Hl0Abe1a6lt_bB/view?usp=sharing";
     window.open(resumeURL, "_blank");
   };
 
