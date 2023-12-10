14daf6c6dc2d8b6a17b4ed0f76c3aaa85c397f23
Adwaith Nair <adwaith1310@gmail.com>
2023-12-11 00:09:02 +0530

    added speed-insights

--- mywebsite/src/app/layout.js
+++ mywebsite/src/app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata = {
   title: "Adwaith Nair",
   description: "This website is created using Next.js",
@@ -6,7 +7,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
